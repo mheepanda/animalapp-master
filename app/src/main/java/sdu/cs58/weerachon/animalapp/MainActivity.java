37ce766dc102d1891ac7bc5cbb3ff011a600bb2a
jassadakon <jassadakon@gmail.com>
2018-04-25 13:58:52 +0700

    Initial commit

---
+++ app/src/main/java/sdu/cs58/weerachon/animalapp/MainActivity.java
@@ -0,0 +1,49 @@
+package sdu.cs58.weerachon.animalapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //
+    EditText nameEditText;
+    Button startButton;
+    String nameString;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameEditText = findViewById(R.id.edtName);
+        startButton = findViewById(R.id.btnStart);
+
+        startButton .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nameString = nameEditText.getText().toString().trim();///การเอาข้อมุลในedtมาเก็บในnamestring
+                if (nameString.length() == 0) {
+                    Toast.makeText(getApplicationContext(), "กรุณาใส่ชื่อ", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    //เปิดหน้า game
+                    Intent startIntent = new Intent(MainActivity.this, GameActivity.class);
+                    startIntent.putExtra("Name", nameString);
+                    startActivity(startIntent);
+                }
+
+
+            }
+        });
+
+
+    }
+
+
+}
+
